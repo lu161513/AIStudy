f7dd6eefee5eccb5f5ede21423df2621bdf524b6
lutao17 <lutao5@jd.com>
2017-05-12 16:09:24 +0800

    提交gitignore，修正gbdt.py中的符号

--- GBDT/gbdt.py
+++ GBDT/gbdt.py
@@ -9,7 +9,7 @@ import numpy as np
 import cPickle as pickle
 
 reload(sys)
-sys.setdefaultencoding(‘utf8’)
+sys.setdefaultencoding('utf8')
 
 # Multinomial Naive Bayes Classifier
 def naive_bayes_classifier(train_x, train_y):
@@ -30,7 +30,7 @@ def knn_classifier(train_x, train_y):
 # Logistic Regression Classifier
 def logistic_regression_classifier(train_x, train_y):
     from sklearn.linear_model import LogisticRegression
-    model = LogisticRegression(penalty=‘l2’)
+    model = LogisticRegression(penalty='l2')
     model.fit(train_x, train_y)
     return model
 
@@ -62,7 +62,7 @@ def gradient_boosting_classifier(train_x, train_y):
 # SVM Classifier
 def svm_classifier(train_x, train_y):
     from sklearn.svm import SVC
-    model = SVC(kernel=‘rbf’, probability=True)
+    model = SVC(kernel='rbf', probability=True)
     model.fit(train_x, train_y)
     return model
 
@@ -70,20 +70,20 @@ def svm_classifier(train_x, train_y):
 def svm_cross_validation(train_x, train_y):
     from sklearn.grid_search import GridSearchCV
     from sklearn.svm import SVC
-    model = SVC(kernel=‘rbf’, probability=True)
-    param_grid = {‘C’: [1e–3, 1e–2, 1e–1, 1, 10, 100, 1000], ‘gamma’: [0.001, 0.0001]}
+    model = SVC(kernel='rbf', probability=True)
+    param_grid = {'C' :[1e-3, 1e-2, 1e-1, 1, 10, 100, 1000], 'gamma':[0.001, 0.0001]}
     grid_search = GridSearchCV(model, param_grid, n_jobs = 1, verbose=1)
     grid_search.fit(train_x, train_y)
     best_parameters = grid_search.best_estimator_.get_params()
     for para, val in best_parameters.items():
         print para, val
-    model = SVC(kernel=‘rbf’, C=best_parameters[‘C’], gamma=best_parameters[‘gamma’], probability=True)
+    model = SVC(kernel='rbf', C=best_parameters['C'], gamma=best_parameters['gamma'], probability=True)
     model.fit(train_x, train_y)
     return model
 
 def read_data(data_file):
     import gzip
-    f = gzip.open(data_file, “rb”)
+    f = gzip.open(data_file, "rb")
     train, val, test = pickle.load(f)
     f.close()
     train_x = train[0]
@@ -92,45 +92,45 @@ def read_data(data_file):
     test_y = test[1]
     return train_x, train_y, test_x, test_y
 
-if __name__ == ‘__main__’:
-    data_file = “mnist.pkl.gz”
+if __name__ == '__main__':
+    data_file = "mnist.pkl.gz"
     thresh = 0.5
     model_save_file = None
     model_save = {}
 
-    test_classifiers = [‘NB’, ‘KNN’, ‘LR’, ‘RF’, ‘DT’, ‘SVM’, ‘GBDT’]
-    classifiers = {‘NB’:naive_bayes_classifier,
-                  ‘KNN’:knn_classifier,
-                   ‘LR’:logistic_regression_classifier,
-                   ‘RF’:random_forest_classifier,
-                   ‘DT’:decision_tree_classifier,
-                  ‘SVM’:svm_classifier,
-                ‘SVMCV’:svm_cross_validation,
-                 ‘GBDT’:gradient_boosting_classifier
+    test_classifiers = ['NB', 'KNN', 'LR', 'RF', 'DT', 'SVM', 'GBDT']
+    classifiers = {'NB':naive_bayes_classifier,
+                  'KNN':knn_classifier,
+                   'LR':logistic_regression_classifier,
+                   'RF':random_forest_classifier,
+                   'DT':decision_tree_classifier,
+                  'SVM':svm_classifier,
+                'SVMCV':svm_cross_validation,
+                 'GBDT':gradient_boosting_classifier
     }
 
-    print ‘reading training and testing data…’
+    print 'reading training and testing data…'
     train_x, train_y, test_x, test_y = read_data(data_file)
     num_train, num_feat = train_x.shape
     num_test, num_feat = test_x.shape
     is_binary_class = (len(np.unique(train_y)) == 2)
-    print ‘******************** Data Info *********************’
-    print ‘#training data: %d, #testing_data: %d, dimension: %d’ % (num_train, num_test, num_feat)
+    print '******************** Data Info *********************'
+    print '#training data: %d, #testing_data: %d, dimension: %d' % (num_train, num_test, num_feat)
 
     for classifier in test_classifiers:
-        print ‘******************* %s ********************’ % classifier
+        print '******************* %s ********************' % classifier
         start_time = time.time()
         model = classifiers[classifier](train_x, train_y)
-        print ‘training took %fs!’ % (time.time() – start_time)
+        print 'training took %fs!' % (time.time()-start_time)
         predict = model.predict(test_x)
         if model_save_file != None:
             model_save[classifier] = model
         if is_binary_class:
             precision = metrics.precision_score(test_y, predict)
             recall = metrics.recall_score(test_y, predict)
-            print ‘precision: %.2f%%, recall: %.2f%%’ % (100 * precision, 100 * recall)
+            print 'precision: %.2f%%, recall: %.2f%%' % (100 * precision, 100 * recall)
         accuracy = metrics.accuracy_score(test_y, predict)
-        print ‘accuracy: %.2f%%’ % (100 * accuracy)
+        print 'accuracy: %.2f%%' % (100 * accuracy)
 
     if model_save_file != None:
-        pickle.dump(model_save, open(model_save_file, ‘wb’))
+        pickle.dump(model_save, open(model_save_file, 'wb'))
